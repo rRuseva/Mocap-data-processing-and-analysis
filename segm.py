320593c01c5c6c3cc8da8120cfe6f02688a280b9
Radina Ruseva <ruseva.radina@gmail.com>
2018-07-23 14:10:07 +0300

    segm.py and rest-pose-analysys.py renamed

---
+++ segm.py
@@ -0,0 +1,275 @@
+import os
+import sys
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from scipy import stats
+from scipy.signal import argrelextrema
+sys.path.insert(0, "D:\Radi\Radi RU\4ti kurs\2sm-WBU\MOCAP\Python\mocap")
+# import mocap_tools
+
+import tools as t
+
+if __name__ == "__main__":
+	dictionary_path = ''
+	###
+	#
+	# load whole take file with start/end frame without T-poses
+	#
+	###
+
+	filename = os.path.join(dictionary_path, 'projevy_pocasi_01_ob_rh_lh_b_g_face_gaps.c3d')
+	title = 'Pocasi_01'
+	start_frame = 600
+	end_frame = 7445
+
+	# start_frame = 1000
+	# end_frame = 1710
+
+
+	# filename = os.path.join(dictionary_path, 'projevy_pocasi_02_ob_rh_lh_b_g_face_gaps.c3d')
+	# title = 'Pocasi_02'
+	# start_frame = 608
+	# end_frame = 6716
+	
+	# #end_frame = 1200
+	# start_frame = 2500
+	# end_frame = 3500
+	
+	# filename = os.path.join(dictionary_path, 'projevy_pocasi_03_ob_rh_lh_b_g_face_gaps.c3d')
+	# title = 'Pocasi_03'
+	# start_frame = 743
+	# end_frame = 6680
+
+	# filename = os.path.join(dictionary_path, 'projevy_pocasi_04_ob_rh_lh_b_g_face_gaps.c3d')
+	# title = 'Pocasi_04'
+	# start_frame = 600
+	# end_frame = 5115
+
+	data, marker_list, fps = t.read_frames(filename)
+	
+	###
+	#
+	# change origin point to be between the hip's markers // True is to find the relative coordinates
+	#
+	###
+
+	new_origin = ['RFWT', 'LFWT', 'RBWT']
+	new_data = t.change_origin_point(data, new_origin, marker_list, True)
+	
+	###
+	# compute velocity based on original trajectory 
+	# returns - 3 chanel (x,y,z) velocity and normilized velocity
+	###	
+	r_velocity, r_vel = t.hand_velocity(start_frame, end_frame, new_data, marker_list, 'R')
+	# compute the median value vor velocity used as threshold for later analysis and segmentation
+	median = np.median(r_vel)
+
+	# compute acceleration based on normilized velocity
+	r_acc = t.hand_acceleration(r_vel)
+	# low pass filter of the acceleration for removing noise coused by recording techology
+	r_acc_filt = t.butter_filter(r_acc, 12, fps, 10)
+	
+	###
+	# raw segmentation of whole take
+	# finding start and end frame of signs (exiting rest pose - entering rest pose)
+	###	
+	zero_crossing = t.zero_crossing(r_acc_filt)
+
+	labels = t.segm(r_vel, r_acc_filt, start_frame, end_frame, new_data, marker_list, median)
+	signs, signs1 = t.get_signs_borders(labels, start_frame, end_frame)
+
+
+	count = len(signs)
+	# analyze velocity during rest pose for better defining the threshold used for segmentation 
+	rest_pose_vel = np.zeros([end_frame - start_frame])
+	for i in range(0, count-1):
+		st = signs[i][1]
+		en = signs[i+1][0] 
+		n = en-st
+
+		vel, vel_norm = t.hand_velocity(st+start_frame,en+start_frame,new_data, marker_list,'R')
+		rest_pose_vel[st:en] = vel_norm
+
+	tr = np.amax(rest_pose_vel)
+	print("threshold=", tr)
+
+	# refined starts and ends of the signs 
+	# labels2 = t.segm(r_vel, r_acc_filt, start_frame, end_frame, new_data, marker_list, tr)
+	# signs2, signs3 = t.get_signs_borders(labels2, start_frame, end_frame)
+	signs2, signs3 = t.segment_signs(start_frame, end_frame, new_data,marker_list,fps, tr)
+	count2 = len(signs2)
+
+	###
+	### test single sign
+	###
+	# st = 1729
+	# en = 1998
+	# diff_right_hand, diff_left_hand, diff_RWRE, diff_LWRE, right_dominant, one_hand = t.hand_displacment(st, en, new_data, marker_list)
+	# print("*****")
+	# # print(sign[0])
+	# message = "The sign between {} - {} frames is".format(st, en)
+	# if(one_hand == 3):
+	# 	message = message + ' two handed'
+	# elif(one_hand == 1):
+	# 	message = message + ' right handed'
+	# elif(one_hand == 2):
+	# 	message = message + " left handed"
+	# else:
+	# 	message = message + " with no hands"
+
+	# print(message)
+	# h='R'
+	# if(right_dominant == 1):
+	# 	print("Dominant hand is: Right \n")
+	# else:
+	# 	print("Dominant hand is: Left hand \n")
+	# 	h='L'
+
+
+	# vel_norm = r_vel[st-start_frame:en-start_frame]
+	# acc = r_acc[st-start_frame:en-start_frame]
+	# acc_f = r_acc_filt[st-start_frame:en-start_frame]
+	
+	# # zeros = t.zero_crossing(acc_f)
+	# # extr = t.find_extremums(acc_f)
+	# zeros = t.interesting_points(acc_f)
+	# # print(zeros)
+	# # print(np.shape(zeros))
+	# start = zeros[1]
+	# end = zeros[np.shape(zeros)[0]-3]
+	# print('Real start and end')
+	# print("{}-{}".format(start+st, end+st))
+	
+	# # print(zeros+start_frame)
+	# l = t.segm(vel_norm, acc_f, st-start_frame, en-start_frame, new_data, marker_list, tr)
+	# start1, end1 = t.get_real_signs(vel_norm, l, st, en )
+	# print('-Real start 1 and end 1')
+	# print("{}-{}".format(start1+st, end1+st))
+	# # ss, ss1 = t.get_signs_borders(l, start_frame, end_frame)
+	# # print(ss.shape)
+	# # c = len(ss)
+	# print()
+
+	# x = np.arange(st, en)
+
+	# fig1 = plt.figure("{}-{}-{}sign-vel".format(title,st,en), figsize=(10.5,7))
+	# fig1.suptitle("Hand velocity for sign between {} and {} frame".format(st, en))	
+
+	# plt.plot(x, vel_norm, 'c', label='Normilized velocity') 
+	# plt.plot(x, acc, 'm', label='Filtered acc') 
+	# plt.plot(x[zeros], vel_norm[zeros], 'o')
+	# # plt.plot(x[extr], vel_norm[extr], 'mo')
+
+
+	# plt.plot(x[start], vel_norm[start], 'rs', label="Start")
+	# plt.plot(x[end], vel_norm[end], 'r*', label= "End")
+
+	# plt.plot(x[start1], vel_norm[start1], 'gs', label="Start1")
+	# plt.plot(x[end1], vel_norm[end1], 'g*', label= "End1")
+	# # plt.plot(x[ss[:, 0]], r_vel[ss[:, 0]], 'rs', label = "Start")	
+	# # plt.plot(x[ss[:, 1]], r_vel[ss[:, 1]], 'r*', label = "End")	
+
+	# # plt.plot(x[s1[:, 0]], r_vel[s1[:, 0]], 'gs', label = "Start")	
+	# # plt.plot(x[s1[:, 1]], r_vel[s1[:, 1]], 'g*', label = "End")
+
+	# plt.axhline(y=tr, color='r', linestyle='-', label="Treshold")
+	# plt.ylabel("Velocity (mm/frame)") 
+	# plt.xlabel("Frames")
+	# plt.grid(True)
+	# legend = fig1.legend(loc='upper right')
+	# # t.plot_hand_location(st, en, new_data, marker_list)
+	# plt.show()
+
+
+
+
+
+
+
+	###
+	#
+	# analysis of each sign beased on refined raw segmentation 
+	# 
+	###	
+
+	for sign in enumerate(signs2):
+		st = sign[1][0]+start_frame
+		en = sign[1][1]+start_frame
+
+		diff_right_hand, diff_left_hand, diff_RWRE, diff_LWRE, right_dominant, one_hand = t.hand_displacment(st, en, new_data, marker_list)
+		print("*****")
+		print(sign[0]+1)
+		message = "{} The sign between {} - {} frames is".format(sign[0]+1, st, en)
+		if(one_hand == 3):
+			message = message + ' two handed'
+		elif(one_hand == 1):
+			message = message + ' right handed'
+		elif(one_hand == 2):
+			message = message + " left handed"
+		else:
+			message = message + " with no hands"
+
+		print(message)
+		h='R'
+		if(right_dominant == 1):
+			print("Dominant hand is: Right \n")
+		else:
+			print("Dominant hand is: Left hand \n")
+			h='L'
+
+
+		vel_norm = r_vel[st-start_frame:en-start_frame]
+		acc = r_acc[st-start_frame:en-start_frame]
+		acc_f = r_acc_filt[st-start_frame:en-start_frame]
+		
+		zeros = t.zero_crossing(acc_f)
+		zeros1 = t.interesting_points(acc_f)
+		# print(zeros)
+		# print(np.shape(zeros))
+		start = zeros[1]
+		end = zeros[np.shape(zeros)[0]-3]
+		print('Real start and end')
+		print("{}-{}".format(start+st, end+st))
+		
+		# print(zeros+start_frame)
+		l = t.segm(vel_norm, acc_f, st-start_frame, en-start_frame, new_data, marker_list, tr)
+		start1, end1 = t.get_real_signs(vel_norm, l, st, en )
+		print('-Real start 1 and end 1')
+		print("{}-{}".format(start1+st, end1+st))
+		# ss, ss1 = t.get_signs_borders(l, start_frame, end_frame)
+		# print(ss.shape)
+		# c = len(ss)
+		print()
+
+		x = np.arange(st, en)
+
+		fig1 = plt.figure("{}_{}-{}-{}sign-vel".format(sign[0]+1,title,st,en), figsize=(10.5,7))
+		fig1.suptitle("{}. Hand velocity for sign between {} and {} frame".format(sign[0]+1, st, en))	
+
+		plt.plot(x, vel_norm, 'c', label='Normilized velocity') 
+		# plt.plot(x, acc, 'm', label='Acc') 
+		plt.plot(x, acc_f, 'y', label='Filtered acc') 
+		plt.plot(x[zeros1], vel_norm[zeros1], 'mo')
+		plt.plot(x[zeros], vel_norm[zeros], 'o')
+
+		plt.plot(x[start], vel_norm[start], 'rs', label="Start")
+		plt.plot(x[end], vel_norm[end], 'r*', label= "End")
+
+		plt.plot(x[start1], vel_norm[start1], 'gs', label="Start1")
+		plt.plot(x[end1], vel_norm[end1], 'g*', label= "End1")
+		
+
+		plt.axhline(y=tr, color='r', linestyle='-', label="Treshold")
+		plt.ylabel("Velocity (mm/frame)") 
+		plt.xlabel("Frames")
+		plt.grid(True)
+		legend = fig1.legend(loc='upper right')
+		# t.plot_hand_location(st, en, new_data, marker_list)
+	plt.show()
+
+
+	# print(signs[8][0]+start_frame)
+	# t.plot_hand_location(signs[8][0]+start_frame, signs[8][1]+start_frame, new_data, marker_list)
+
+
